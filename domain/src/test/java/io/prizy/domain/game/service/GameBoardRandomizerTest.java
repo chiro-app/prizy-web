c7b5030e1456c65d22b1553b283c04d5eff7cb24
Nidhal Dogga <nidhal.d@bankin.com>
2022-07-18 16:22:10 +0100

    prevent generating juxtaposed start and end positions in game boards

---
+++ domain/src/test/java/io/prizy/domain/game/service/GameBoardRandomizerTest.java
@@ -0,0 +1,36 @@
+package io.prizy.domain.game.service;
+
+
+import io.prizy.domain.game.model.GameBoard;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Nidhal Dogga
+ * @created 18/07/2022 17:03
+ */
+
+class GameBoardRandomizerTest {
+
+  private final GameBoardRandomizer randomizer = new GameBoardRandomizer();
+  private final GameBoard board = GameBoard.builder()
+    .rowSize(4)
+    .cells(new Integer[]{
+      1, 1, 1, 1,
+      1, 1, 1, 1,
+      1, 1, 1, 1,
+      1, 1, 1, 1
+    })
+    .build();
+
+  @RepeatedTest(100)
+  @DisplayName("Should generate random points with minimum distance to excluded ones")
+  void shouldRespectMinimumDistance() {
+    var point = randomizer.randomAvailablePosition(board, 2, new Integer[]{0});
+    assertThat(point).isIn(3, 7, 11, 12, 13, 14, 15);
+    point = randomizer.randomAvailablePosition(board, 2, new Integer[]{0});
+  }
+
+}
