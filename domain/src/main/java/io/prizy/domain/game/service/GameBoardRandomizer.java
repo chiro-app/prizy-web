24dbd89a4de18c9ec2d964bcb2c39bfd4d8951da
Nidhal Dogga <nidhal.d@bankin.com>
2022-07-21 13:45:21 +0100

    revert commit c7b5030e1456c65d22b1553b283c04d5eff7cb24

--- domain/src/main/java/io/prizy/domain/game/service/GameBoardRandomizer.java
+++ domain/src/main/java/io/prizy/domain/game/service/GameBoardRandomizer.java
@@ -8,12 +8,9 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 import io.prizy.domain.game.model.GameBoard;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Component;
-
-import static java.lang.Math.floor;
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
 
 /**
  * @author Nidhal Dogga
@@ -22,32 +19,17 @@ import static java.lang.Math.sqrt;
 
 
 @Component
+@RequiredArgsConstructor
 public class GameBoardRandomizer {
 
   private static final Random RANDOM = ThreadLocalRandom.current();
 
-  public Integer randomAvailablePosition(GameBoard board, Integer minimumMoves, Integer... excluded) {
+  public Integer randomAvailablePosition(GameBoard board, Integer... excluded) {
     var availablePositions = IntStream
       .range(0, board.cells().length)
       .filter(index -> board.cells()[index] == 1 && Arrays.stream(excluded).noneMatch(i -> i == index))
-      .filter(index -> Arrays
-        .stream(excluded)
-        .allMatch(excludedIndex -> {
-          var dx = (index % board.rowSize()) - (excludedIndex % board.rowSize());
-          var dy = (index / board.rowSize()) - (excludedIndex / board.rowSize());
-          var distance = (Double) floor(sqrt(pow(dx, 2) + pow(dy, 2)));
-          return distance.longValue() > minimumMoves;
-        }))
       .toArray();
     return availablePositions[RANDOM.nextInt(availablePositions.length - 1)];
-  }
-
-  public Integer randomAvailablePosition(GameBoard board, Integer... excluded) {
-    return randomAvailablePosition(board, 1, excluded);
-  }
-
-  public Integer randomAvailablePosition(GameBoard board, Integer excluded) {
-    return randomAvailablePosition(board, new Integer[]{excluded});
   }
 
   public Pair<Integer, Integer> randomStartEndPosition(GameBoard board) {
--- domain/src/test/java/io/prizy/domain/game/service/GameBoardRandomizerTest.java
+++ domain/src/test/java/io/prizy/domain/game/service/GameBoardRandomizerTest.java
@@ -5,8 +5,6 @@ import io.prizy.domain.game.model.GameBoard;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
  * @author Nidhal Dogga
  * @created 18/07/2022 17:03
@@ -28,8 +26,8 @@ class GameBoardRandomizerTest {
   @RepeatedTest(100)
   @DisplayName("Should generate random points with minimum distance to excluded ones")
   void shouldRespectMinimumDistance() {
-    var point = randomizer.randomAvailablePosition(board, 2, new Integer[]{0});
-    assertThat(point).isIn(3, 7, 11, 12, 13, 14, 15);
+//    var point = randomizer.randomAvailablePosition(board, 2, new Integer[]{0});
+//    assertThat(point).isIn(3, 7, 11, 12, 13, 14, 15);
   }
 
 }
